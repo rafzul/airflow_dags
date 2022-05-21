7cd4a5a20d5d962c06940c7b4b9ea75b420a6201
rafzul <rafi.zulfahmi@mail.ugm.ac.id>
2022-05-21 21:10:31 +0700

    sama

--- download_data_dag.py
+++ download_data_dag.py
@@ -6,12 +6,16 @@ from scripts.parquetizing import parquetize_data
 
 #file path, url and type setup
 
+#getting month and year
+logical_date = "{{ ds }}"
+MONTH = datetime.strptime(logical_date, "%m")
+YEAR = datetime.strptime(logical_date, "%y")
+
 #setting up Bash parametrization
-TAXI_TYPE="yellow"
 URL_PREFIX="https://s3.amazonaws.com/nyc-tlc/trip+data"
 
 #setup download data script path
-bash_datadownload ="../scripts/download_data.sh"
+BASH_DATADOWNLOAD="../scripts/download_data.sh"
 
 #setup download_data path
 # FMONTH= `printf "%02d" ${MONTH}`
@@ -23,15 +27,8 @@ LOCAL_PATH="${LOCAL_PREFIX}/${LOCAL_FILE}"
 #schema file path setup
 SCHEMA_FILEPATH="/schemas/nytaxi_schema_{TAXI_TYPE}"
 
-
-#getting month and year
-logical_date = "{{ ds }}"
-MONTH = datetime.strptime(logical_date, "%m")
-YEAR = datetime.strptime(logical_date, "%y")
-
 #setting up external script path
 EXTSCRIPT_PATH = "../scripts/"
-
 
 #setting up DAG
 default_args = {"owner": "rafzul",
@@ -52,26 +49,25 @@ with DAG(
 
     # for MONTH in {1..12}: ini didefine di schedule_interval buat jaraknya, trus define start_date dan end_date buat start dan mulenya
 
-    # for TAXI_TYPE in {yellow,green}:
-
-    download_data_task = BashOperator(
-        task_id='download_data',
-        bash_command=bash_datadownload,
-        params= {"TAXI_TYPE": TAXI_TYPE, "YEAR": YEAR, "MONTH": MONTH, "URL_PREFIX": URL_PREFIX},        
-    )
-
-    with open(SCHEMA_FILEPATH, 'r') as schema_file:
-        #read the schema file content
-        schema_string = schema_file.read()
-        #call the parquetizing function
-        parquetize_data_task = PythonOperator(
-            task_id="parquetize_data",
-            python_callable=parquetize_data,
-            op_kwargs={
-                "schema": schema_file,
-                "csv_file": LOCAL_PATH,
-            },
+    for TAXI_TYPE in {"yellow","green"}:
+        download_data_task = BashOperator(
+            task_id='download_data',
+            bash_command=BASH_DATADOWNLOAD,
+            params= {"URL": URL,"LOCAL_PREFIX": LOCAL_PREFIX, "LOCAL_PATH": LOCAL_PATH},        
         )
+
+        with open(SCHEMA_FILEPATH, 'r') as schema_file:
+            #read the schema file content
+            schema_string = schema_file.read()
+            #call the parquetizing function
+            parquetize_data_task = PythonOperator(
+                task_id="parquetize_data",
+                python_callable=parquetize_data,
+                op_kwargs={
+                    "schema": schema_file,
+                    "csv_file": LOCAL_PATH,
+                },
+            )
 
 
     download_data_task >> parquetize_data_task
