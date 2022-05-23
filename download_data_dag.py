f6263ee59245ab00f659342ad2bcef4d147899a9
rafzul <rafi.zulfahmi@mail.ugm.ac.id>
2022-05-23 10:32:41 +0700

    fixing task group

--- download_data_dag.py
+++ download_data_dag.py
@@ -50,7 +50,7 @@ with DAG(
     # for MONTH in {1..12}: ini didefine di schedule_interval buat jaraknya, trus define start_date dan end_date buat start dan mulenya
 
     for TAXI_TYPE in {"yellow","green"}:
-        with TaskGroup(group_id=f"download_&_parquetize_{TAXI_TYPE}") as tg1:
+        with TaskGroup(group_id=f"downloadparquetizegroup_{TAXI_TYPE}") as tg1:
             download_data_task = BashOperator   (
                 task_id='download_data',
                 bash_command=BASH_DATADOWNLOAD,
