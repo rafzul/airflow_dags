6f6cdce5502f7f2089718819aff1f81743f92924
rafzul <rafi.zulfahmi@mail.ugm.ac.id>
2022-05-31 19:32:40 +0700

    salah path

--- download_data_dag.py
+++ download_data_dag.py
@@ -26,6 +26,9 @@ LOCAL_PREFIX="/tmp/nytaxidata/${TAXI_TYPE}/${YEAR}/${MONTH}"
 LOCAL_FILE="${TAXI_TYPE}_tripdata_${YEAR}-${FMONTH}.csv"
 LOCAL_PATH="${LOCAL_PREFIX}/${LOCAL_FILE}"
 
+#schemas
+#/opt/airflow/dags/repo/schemas/nytaxi_schema_{TAXI_TYPE}
+
 
 # #setting up external script path
 # EXTSCRIPT_PATH = "/scripts/"
@@ -53,7 +56,7 @@ with DAG(
         with TaskGroup(group_id=f"downloadparquetizegroup_{TAXI_TYPE}") as tg1:
             download_data_task = BashOperator(
                 task_id='download_data',
-                bash_command='/opt/airflow/dags/repo/schemas/nytaxi_schema_{TAXI_TYPE} ',
+                bash_command='/opt/airflow/dags/repo/scripts/download_data.sh ',
                 params= {"URL": URL,"LOCAL_PREFIX": LOCAL_PREFIX, "LOCAL_PATH": LOCAL_PATH},        
             )
 
