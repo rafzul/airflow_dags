2a7e469723745c6c25713b32012eabc5e20da3a6
rafzul <rafi.zulfahmi@mail.ugm.ac.id>
2022-06-05 21:15:39 +0700

    fixing macros import and experiment with calling the variables outside the dag call

--- download_data_dag.py
+++ download_data_dag.py
@@ -55,7 +55,7 @@ with DAG(
 
     # for MONTH in {1..12}: ini didefine di schedule_interval buat jaraknya, trus define start_date dan end_date buat start dan mulenya
 
-    for TAXI_TYPE in {"yellow","green"}
+    for TAXI_TYPE in {"yellow","green"}:
 
         with TaskGroup(group_id=f"downloadparquetizegroup_{TAXI_TYPE}") as tg1:
             download_data_task = BashOperator(
