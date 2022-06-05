400076088908e222602f2df8f8bbff8d1804ad70
rafzul <rafi.zulfahmi@mail.ugm.ac.id>
2022-06-05 20:49:34 +0700

    fix month and year not rendering in jinja templating

--- download_data_dag.py
+++ download_data_dag.py
@@ -10,8 +10,8 @@ import pendulum
 
 #getting month and year
 # logical_date = "{{ ds }}"
-MONTH = "{{ macros.ds_format(ds, "%Y-%m-%d", "%m") }}"
-YEAR = "{{ macros.ds_format(ds, "%Y-%m-%d", "%Y") }}"
+MONTH = "{{ ds_format(ds, "%Y-%m-%d", "%m") }}"
+YEAR = "{{ ds_format(ds, "%Y-%m-%d", "%Y") }}"
 
 #setting up Bash parametrization
 URL_PREFIX="https://s3.amazonaws.com/nyc-tlc/trip+data"
